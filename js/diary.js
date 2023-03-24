6878ca6297f7dfe9fdeb62122beb98bed06fdc4c
taurin360 <126446288+taurin360@users.noreply.github.com>
2023-03-24 14:57:07 +0900

    Add files via upload

--- js/diary.js
+++ js/diary.js
@@ -47,16 +47,16 @@
 
   // タイマのコールバック関数 日記画面表示0.1秒後に実施
   let imgchange = function() {
-    document.querySelectorAll('img').forEach((img_diary) => {
+    document.querySelectorAll('img').forEach((img) => {
       // 日付のIDが一致したら
-      if (img_diary.getAttribute('alt') === diaryId) {
+      if (img.getAttribute('alt') === diaryId) {
         // 表示する日記画像を透過無しにする(CSSの設定で透過無し完了まで2秒かける)
-        img_diary.classList.add('changed');
+        img.classList.add('changed');
         // トランジション動作が完全に終わるまで待つ
         setTimeout(opendisp, 2500);
       } else {
         // 表示画像以外は透過ありにする
-        img_diary.classList.remove('changed');
+        img.classList.remove('changed');
       }
     });
   }
@@ -79,14 +79,26 @@
           diary.classList.remove('done');
           // ヘッダ部分を表示する
           hedContents.classList.remove('done');
-          // IMG画像のときのみ透過、ハンバーガーメニューボタン消去を行う
-          if(diary.children[1].nodeName === 'IMG') {
-            // ハンバーガーメニューボタン非表示にする。
-            open.textContent = '';
-            //  0.1秒後に透過無し画像にする
-            setTimeout(imgchange, 100);
+          // 日記画面が１画面
+          if (diary.childElementCount <= 3) {
+            // IMG画像のとき
+            if (diary.children[1].nodeName === 'IMG') {
+              // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
+              open.textContent = '';
+              // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
+              setTimeout(imgchange, 100);
+            }
+          // 日記画面が２画面
+          } else if (diary.childElementCount <= 5) {
+            // IMG画像が含まれるとき
+            if (diary.children[1].nodeName === 'IMG' || diary.children[3].nodeName === 'IMG') {
+              // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
+              open.textContent = '';
+              // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
+              setTimeout(imgchange, 100);
+            }
           }
-          // 動画の場合はハンバーガーメニューボタンも表示しないし、画像も透過しない。
+          // 動画のみの場合はハンバーガーメニューボタンも表示しないし、画像も透過しない。
         }
         // 押されたボタンの日記画面以外
         else {
@@ -97,15 +109,29 @@
     }
   });
 
+  // 先頭日付画面
+  let diary = document.querySelector('.diary');
   // 日付IDを先頭日記画面のIDで初期化
-  diaryId = document.querySelector('.diary').getAttribute('id');
-  // IMG画像のときのみ透過、ハンバーガーメニューボタン消去を行う
-  if (document.querySelector('.diary').children[1].nodeName === 'IMG') {
-    // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
-    open.textContent = '';
-    // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
-    setTimeout(imgchange, 100);
+  diaryId = diary.getAttribute('id');
+  // 日記画面が１画面
+  if (diary.childElementCount <= 3) {
+    // IMG画像のとき
+    if (diary.children[1].nodeName === 'IMG') {
+      // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
+      open.textContent = '';
+      // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
+      setTimeout(imgchange, 100);
+    }
+  // 日記画面が２画面
+  } else if (diary.childElementCount <= 5) {
+    // IMG画像が含まれるとき
+    if (diary.children[1].nodeName === 'IMG' || diary.children[3].nodeName === 'IMG') {
+      // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
+      open.textContent = '';
+      // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
+      setTimeout(imgchange, 100);
+    }
   }
-  // 動画の場合はハンバーガーメニューボタンも表示しないし、画像も透過しない。
+  // 動画のみの場合はハンバーガーメニューボタンも表示しないし、画像も透過しない。
 
 }
