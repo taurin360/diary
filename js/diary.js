525c6675c202b8d2f890772b1428b8cafb1608c0
taurin360 <126446288+taurin360@users.noreply.github.com>
2023-03-29 10:27:51 +0900

    Add files via upload

--- js/diary.js
+++ js/diary.js
@@ -3,26 +3,22 @@
 { 
   // 初回表示のインデックスは0番目
   let disp_index = 0;
-  let diaryId;
-  let timerId;
-  // タイマ停止中で初期化
-  let gbExe = 0;
-  let regbMsg;
+  // いいね数
+  let gnum = [];
+  // よくないね数
+  let bnum = [];
   const open = document.getElementById('open');
   const close = document.getElementById('close');
   const hedContents = document.querySelector('.hedContents');
   const overlay = document.querySelector('.overlay');
   const good = document.querySelector('.good');
   const bad = document.querySelector('.bad');
-  const gbmsg = document.querySelector('.gb-msg');
   const container = document.querySelector('.container');
+  const gCounter = document.querySelector('.g-cnt');
+  const bCounter = document.querySelector('.b-cnt');
   
-    // ハンバーガーメニュークリック時の動作
+  // ハンバーガーメニュークリック時の動作
   let showHmenu = function() {
-    // タイマ停止とメッセージ消去
-    clearInterval(timerId);
-    gbmsg.textContent = '';
-    gbExe = 0;
     // 日付メニューを前面に表示
     overlay.classList.add('show');
     // ヘッダ部分を非表示とする
@@ -32,12 +28,9 @@
     diary[disp_index].classList.add('done');
     // いいね。よくないね。を非表示
     container.classList.add('done');
-    // IMG画像のとき
-    if (diary[disp_index].children[1].nodeName === 'IMG') {
-      // 表示画像を透過ありに戻す
-      diary[disp_index].children[1].classList.remove('changed');
-    } else {
-      // 動画のとき停止
+    // 動画のとき
+    if (diary[disp_index].children[1].nodeName === 'VIDEO') {
+      // 動画停止
       diary[disp_index].children[1].pause();
       // 再生を先頭に戻す
       diary[disp_index].children[1].currentTime = 0;
@@ -45,11 +38,8 @@
     // 日記画面が2画面
     if (diary[disp_index].childElementCount === 5) {
       // IMG画像のとき
-      if (diary[disp_index].children[3].nodeName === 'IMG') {
-        // 表示画像を透過ありに戻す
-        diary[disp_index].children[3].classList.remove('changed');
-      } else {
-        // 動画のとき停止
+      if (diary[disp_index].children[3].nodeName === 'VIDEO') {
+        // 動画停止
         diary[disp_index].children[1].pause();
         // 再生を先頭に戻す
         diary[disp_index].children[1].currentTime = 0;
@@ -68,50 +58,9 @@
     // 日記画面を表示とする
     const diary = document.querySelectorAll('.diary');
     diary[disp_index].classList.remove('done');
-    // いいね。よくないね。を非示
+    // いいね/よくないねを非示
     container.classList.remove('done');
-    // 日記画面が1画面
-    if (diary[disp_index].childElementCount === 3) {
-      // IMG画像のとき
-      if (diary[disp_index].children[1].nodeName === 'IMG') {
-        // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
-        open.textContent = '';
-        // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
-        setTimeout(imgchange, 100);
-      }
-    // 日記画面が2画面
-    } else if (diary[disp_index].childElementCount === 5) {
-      // IMG画像が含まれるとき
-      if (diary[disp_index].children[1].nodeName === 'IMG' || diary[disp_index].children[3].nodeName === 'IMG') {
-        // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
-        open.textContent = '';
-        // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
-        setTimeout(imgchange, 100);
-      }
-    }
   });
-
-  // トランジション動作が完全に終わってから
-  let opendisp = function() {
-    // ハンバーガーメニューボタンを表示
-    open.textContent = 'menu';
-  }
-
-  // タイマのコールバック関数 日記画面表示0.1秒後に実施
-  let imgchange = function() {
-    document.querySelectorAll('img').forEach((img) => {
-      // 日付のIDが一致したら
-      if (img.getAttribute('alt') === diaryId) {
-        // 表示する日記画像を透過無しにする(CSSの設定で透過無し完了まで2秒かける)
-        img.classList.add('changed');
-        // トランジション動作が完全に終わるまで待つ
-        setTimeout(opendisp, 2500);
-      } else {
-        // 表示画像以外は透過ありにする
-        img.classList.remove('changed');
-      }
-    });
-  }
 
   // 日付ボタンのクリックを待ち受ける
   document.querySelector('.day-btn').addEventListener('click', e => {
@@ -126,33 +75,16 @@
           // インデックスを保存
           disp_index = index;
           // 日付のIDを保存
-          diaryId = diary.id;
+          // diaryId = diary.id;
           // 日記画面を表示する
           diary.classList.remove('done');
           // ヘッダ部分を表示する
           hedContents.classList.remove('done');
-          // いいね。よくないね。を非示
+          // いいね/よくないねを非示する
           container.classList.remove('done');
-          // 日記画面が1画面
-          if (diary.childElementCount === 3) {
-            // IMG画像のとき
-            if (diary.children[1].nodeName === 'IMG') {
-              // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
-              open.textContent = '';
-              // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
-              setTimeout(imgchange, 100);
-            }
-          // 日記画面が2画面
-          } else if (diary.childElementCount === 5) {
-            // IMG画像が含まれるとき
-            if (diary.children[1].nodeName === 'IMG' || diary.children[3].nodeName === 'IMG') {
-              // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
-              open.textContent = '';
-              // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
-              setTimeout(imgchange, 100);
-            }
-          }
-          // 動画のみの場合はハンバーガーメニューボタンも表示しないし、画像も透過しない。
+          // いいね/よくないね数を設定
+          gCounter.textContent = gnum[disp_index];
+          bCounter.textContent = bnum[disp_index];
         }
         // 押されたボタンの日記画面以外
         else {
@@ -163,70 +95,25 @@
     }
   });
 
-  let msgErase = function() {
-    // タイマ停止中
-    gbExe = 0;
-    // メッセージクリア
-    gbmsg.textContent = '';
+  // いいね/よくないね数初期化
+  for (let i = 0;  i < 10; i++) {
+    gnum[i] = 0;
+    bnum[i] = 0;
   }
-
     // いいねボタンのクリックを待ち受ける
   good.addEventListener('click', () => {
-    // 実行中は受付けない
-    if (gbExe === 0) {
-      // タイマ実行中
-      gbExe = 1;
-      // メッセージを表示
-      gbmsg.textContent = 'ありがとう！';
-      // 表示時間
-      timerId = setTimeout(msgErase, 4000);
-    } else {
-      // メッセージを上書き
-      regbMsg = gbmsg.textContent;
-      gbmsg.textContent = regbMsg;
-    }
+    // いいね。カウントアップ
+    gnum[disp_index]++;
+    // いいね。カウント数表示
+    gCounter.textContent = `${gnum[disp_index]}`;
   });
 
-  // よくないねボタンのクリックを待ち受ける
+    // よくないねボタンのクリックを待ち受ける
   bad.addEventListener('click', () => {
-    // 実行中は受付けない
-    if (gbExe === 0) {
-      // タイマ実行中
-      gbExe = 1;
-      // メッセージを表示
-      gbmsg.textContent = 'なんだとぉ！';
-      // 表示時間
-      timerId = setTimeout(msgErase, 4000);
-    } else {
-      // メッセージを上書き
-      regbMsg = gbmsg.textContent;
-      gbmsg.textContent = regbMsg;
-    }
+    // よくないね。カウントアップ
+    bnum[disp_index]++;
+    // よくないね。カウント数表示
+    bCounter.textContent = `${bnum[disp_index]}`;
   });
-
-  // 先頭日付画面
-  let diary = document.querySelector('.diary');
-  // 日付IDを先頭日記画面のIDで初期化
-  diaryId = diary.getAttribute('id');
-  // 日記画面が1画面
-  if (diary.childElementCount === 3) {
-    // IMG画像のとき
-    if (diary.children[1].nodeName === 'IMG') {
-      // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
-      open.textContent = '';
-      // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
-      setTimeout(imgchange, 100);
-    }
-  // 日記画面が2画面
-  } else if (diary.childElementCount === 5) {
-    // IMG画像が含まれるとき
-    if (diary.children[1].nodeName === 'IMG' || diary.children[3].nodeName === 'IMG') {
-      // トランジション動作が終わるまでハンバーガーメニューボタン非表示[初回表示用]
-      open.textContent = '';
-      // 一旦透過画像表示し、0.1秒後に透過無し画像にする[初回表示用]
-      setTimeout(imgchange, 100);
-    }
-  }
-  // 動画のみの場合はハンバーガーメニューボタンも表示しないし、画像も透過しない。
 
 }
