84715542a5009354e32024f954e87f3129592daf
taurin360 <126446288+taurin360@users.noreply.github.com>
2023-03-29 22:13:54 +0900

    Add files via upload

--- js/diary.js
+++ js/diary.js
@@ -5,8 +5,6 @@
   let disp_index = 0;
   // いいね数
   let gnum = [];
-  // よくないね数
-  let bnum = [];
   const open = document.getElementById('open');
   const close = document.getElementById('close');
   const hedContents = document.querySelector('.hedContents');
@@ -15,7 +13,6 @@
   const bad = document.querySelector('.bad');
   const container = document.querySelector('.container');
   const gCounter = document.querySelector('.g-cnt');
-  const bCounter = document.querySelector('.b-cnt');
   
   // ハンバーガーメニュークリック時の動作
   let showHmenu = function() {
@@ -26,8 +23,12 @@
     // 現在表示している日記画面を非表示とする
     const diary = document.querySelectorAll('.diary');
     diary[disp_index].classList.add('done');
-    // いいね。よくないね。を非表示
+    // いいね/よくないねを非表示
     container.classList.add('done');
+    // いいね/よくないね音声を停止
+    good_audio.pause();
+    bad_audio.pause();
+
     // 動画のとき
     if (diary[disp_index].children[1].nodeName === 'VIDEO') {
       // 動画停止
@@ -82,9 +83,8 @@
           hedContents.classList.remove('done');
           // いいね/よくないねを非示する
           container.classList.remove('done');
-          // いいね/よくないね数を設定
+          // いいね数を設定
           gCounter.textContent = gnum[disp_index];
-          bCounter.textContent = bnum[disp_index];
         }
         // 押されたボタンの日記画面以外
         else {
@@ -95,25 +95,31 @@
     }
   });
 
-  // いいね/よくないね数初期化
+  // いいね数初期化
   for (let i = 0;  i < 10; i++) {
     gnum[i] = 0;
-    bnum[i] = 0;
   }
-    // いいねボタンのクリックを待ち受ける
+  // いいね音声のインスタンス作成
+  let good_audio = new Audio("audio/good.mp3");
+  let bad_audio = new Audio("audio/bad.mp3");
+  // いいねボタンのクリックを待ち受ける
   good.addEventListener('click', () => {
     // いいね。カウントアップ
     gnum[disp_index]++;
     // いいね。カウント数表示
     gCounter.textContent = `${gnum[disp_index]}`;
+    // いいね音声を頭から再生
+    bad_audio.pause();
+    good_audio.currentTime = 0;
+    good_audio.play();
   });
 
     // よくないねボタンのクリックを待ち受ける
   bad.addEventListener('click', () => {
-    // よくないね。カウントアップ
-    bnum[disp_index]++;
-    // よくないね。カウント数表示
-    bCounter.textContent = `${bnum[disp_index]}`;
+    // よくないね音声を頭から再生
+    good_audio.pause();
+    bad_audio.currentTime = 0;
+    bad_audio.play();
   });
 
 }
