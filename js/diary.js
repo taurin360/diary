f2c9f3dad5f004bcf072596be0fc1128e5c0839d
taurin360 <126446288+taurin360@users.noreply.github.com>
2023-04-08 22:49:46 +0900

    Add files via upload

--- js/diary.js
+++ js/diary.js
@@ -5,91 +5,18 @@
   let disp_index = 0;
   // いいね数
   let gnum = [];
-  const open = document.getElementById('open');
-  const close = document.getElementById('close');
   const good = document.getElementById('good');
   const bad = document.getElementById('bad');
   const gCounter = document.getElementById('g-cnt');
   const bCounter = document.getElementById('b-cnt');
   const diary = document.querySelectorAll('.diary');
-  const hedContents = document.querySelector('.hedContents');
-  const overlay = document.querySelector('.overlay');
+  const menuHeader = document.querySelector('.menu-header');
+  const diaryHeader = document.querySelector('.diary-header');
+  const diaryList = document.querySelector('.diary-list');
   const goodBad = document.querySelector('.good-bad');
   const dayBtn = document.querySelector('.day-btn');
   const historyBtn = document.querySelector('.history');
 
-// ---------------------------------------------------------------------
-// ハンバーガーメニューボタン押下処理
-// ---------------------------------------------------------------------
-  // ハンバーガーメニュークリック時の動作
-  let showHmenu = function() {
-    // 日付メニューを前面に表示
-    overlay.classList.remove('dnone');
-    // ヘッダ部分を非表示とする
-    hedContents.classList.add('dnone');
-    // 更新履歴ボタンを非表示とする
-    historyBtn.classList.add('dnone');
-    // 現在表示している日記画面を非表示とする
-    diary[disp_index].classList.add('dnone');
-    // いいね/よくないねを非表示
-    goodBad.classList.add('dnone');
-    // いいね/よくないね音声を停止
-    good_audio.pause();
-    bad_audio.pause();
-    // タイプライタ表示
-    if (diary[disp_index].children[2].nodeName === 'P' && 
-        diary[disp_index].children[2].classList.contains('d-twr') === true) {
-      // タイプライタ文章を元に戻す
-      diary[disp_index].children[2].textContent = charP;
-      // カウントMAXにしてこれ以上タイプライタ処理を実行させない。
-      charCnt = charLen;
-    }
-    // 動画のとき
-    if (diary[disp_index].children[1].nodeName === 'VIDEO') {
-      // 動画停止
-      diary[disp_index].children[1].pause();
-      // 再生を先頭に戻す
-      diary[disp_index].children[1].currentTime = 0;
-    }
-    // 日記画面が2画面
-    if (diary[disp_index].childElementCount === 5) {
-      // 動画のとき
-      if (diary[disp_index].children[3].nodeName === 'VIDEO') {
-        // 動画停止
-        diary[disp_index].children[3].pause();
-        // 再生を先頭に戻す
-        diary[disp_index].children[3].currentTime = 0;
-      }
-    }
-  }
-  // ハンバーガーメニューボタンのクリックを待ち受ける
-  open.addEventListener('click', showHmenu);
-
-  // ---------------------------------------------------------------------
-  // クローズボタン押下処理
-  // ---------------------------------------------------------------------
-  // クローズボタンのクリックを待ち受ける
-  close.addEventListener('click', () => {
-    // 日付メニューを消す
-    overlay.classList.add('dnone');
-    // ヘッダ部分表示とする
-    hedContents.classList.remove('dnone');
-    // 日記画面を表示とする
-    diary[disp_index].classList.remove('dnone');
-    // いいね/よくないねを非示
-    goodBad.classList.remove('dnone');
-    // 更新履歴ボタンを表示する
-    historyBtn.classList.remove('dnone');
-    // タイプライタ表示
-    if (diary[disp_index].children[2].nodeName === 'P' && 
-        diary[disp_index].children[2].classList.contains('d-twr') === true) {
-      charLen = diary[disp_index].children[2].textContent.length;
-      charP = diary[disp_index].children[2].textContent;
-      diary[disp_index].children[2].textContent = '';
-      charCnt = 0;
-      setTimeout(charSlice, 100);
-    }
-  });
   // ---------------------------------------------------------------------
   // 日付ボタン押下処理
   // ---------------------------------------------------------------------
@@ -98,7 +25,7 @@
     // 押されたのがボタンの時
     if (e.target.nodeName === 'LI') {
       // 日付メニューを消す
-      overlay.classList.add('dnone');
+      diaryList.classList.add('dnone');
       // 日記画面をサーチ
       document.querySelectorAll('.diary').forEach((diary, index) => {
         // 押されたボタンの日記画面なら
@@ -107,8 +34,10 @@
           disp_index = index;
           // 日記画面を表示する
           diary.classList.remove('dnone');
-          // ヘッダ部分を表示する
-          hedContents.classList.remove('dnone');
+          // 先頭画面ヘッダ部分を非表示にする
+          menuHeader.classList.add('dnone');
+          // 日記画面ヘッダ部分を表示する
+          diaryHeader.classList.remove('dnone');
           // 更新履歴ボタンを表示する
           historyBtn.classList.remove('dnone');
           // いいね/よくないねを表示する
@@ -189,12 +118,6 @@
     charCnt++;
     setTimeout(charSlice, 100);
   };
-  // タイプライタ表示のとき
-  if (diary[disp_index].children[2].nodeName === 'P' && 
-      diary[disp_index].children[2].classList.contains('d-twr') === true) {
-    diary[disp_index].children[2].textContent = '';
-    setTimeout(charSlice, 100);
-  }
 
 }
 
